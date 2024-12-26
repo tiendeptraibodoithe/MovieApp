a335cfdc7ef1d27b86dd0cf7ff86bdc4e618de1f
tiendeptraibodoithe <tienhaku0412@gmail.com>
2024-12-26 17:08:52 +0700

    v1

---
+++ app/src/main/java/com/pro/movie/constant/AboutUsConfig.kt
@@ -0,0 +1,8 @@
+package com.pro.movie.constant
+
+interface AboutUsConfig {
+    companion object {
+        const val ABOUT_US_TITLE = "PhimMeme"
+        const val ABOUT_US_SLOGAN = "Cam kết cao về chất lượng phim và dịch vụ tuyệt vời"
+    }
+}
